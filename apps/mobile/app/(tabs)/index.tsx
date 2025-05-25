8c9d6b1e01155bd54b254d22a9114af1f6f676eb
atomisadev <atomisadev@pm.me>
2025-05-25 23:20:09 +0000

    initial push

---
+++ apps/mobile/app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <div className="bg-slate-100 rounded-xl">
+      <p className="text-lg font-medium">Welcome to Tailwind</p>
+    </div>
+  );
+};
